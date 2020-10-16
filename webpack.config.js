af43278a0efc4f18afec55c9829285a6e1dabe69
Jakub Stefko <jstefko@man.poznan.pl>
2020-10-17 00:23:27 +0200

    font added

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,18 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+      {
+        test: /\.(png|jpg|pdf)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
     ],
   },
   resolve: { extensions: ['*', '.js', '.jsx'] },
